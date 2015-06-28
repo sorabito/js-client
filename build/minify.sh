012f01836183449f963c2099b4819cc3d7746fc8
apilayer <support@apilayer.net>
2015-06-28 22:10:02 +0200

    Delete minify.sh

--- build/minify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-version="v1.2.1"
-preamble="/* jsonrates.js $version | Currency exchange rates API | jsonrates.com */"
-input="src/jr.js src/jrequest.js"
-output="jsonrates.min.js"
-
-uglifyjs $input --compress --preamble "$preamble" --stats --output $output
